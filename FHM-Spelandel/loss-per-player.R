40284b159c31783855af1d2d32efdde18daf0b9b
Henrik Hallberg <henrik.hallberg@playscan.com>
2016-02-23 19:45:46 +0100

    Bugfix: Loss per year -- correct wrong ordering

--- FHM-Spelandel/loss-per-player.R
+++ FHM-Spelandel/loss-per-player.R
@@ -1,7 +1,7 @@
 df <- data.frame(year=c(2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007),
                  netloss.M=c(21077, 20570, 20842, 20529, 20265, 19952, 19696, 18500, 17661), #http://www.lotteriinspektionen.se/sv/Om-oss/Statistik/Omsattning-och-behallning-av-spel/Testsida/,
                  p.play=c(0.58, 0.6, 0.61, 0.62, 0.61, 0.62, 0.63, 0.65, 0.67), # http://www.folkhalsomyndigheten.se/documents/statistik-uppfoljning/enkater-undersokningar/nationella-folkhalsoenkaten/resultat-2015/spelvanor-regionala-resultat-2015.xls
-                 population=c(7197776, 7273402, 7347057, 7424838, 7489322,7542245, 7592081, 7644548, 7709612)) # http://www.statistikdatabasen.scb.se/sq/10169
+                 population=c(7709612, 7644548, 7592081, 7542245, 7489322, 7424838, 7347057, 7273402, 7197776)) # http://www.statistikdatabasen.scb.se/sq/10169
 
 
 df$per.player <- with(df, netloss.M*1000000/(population * p.play))
